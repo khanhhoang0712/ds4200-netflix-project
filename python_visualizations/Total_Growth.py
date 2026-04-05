5ec4a81f45301333205fa2e62f3fe18bdbc3f460
opeckittirat <opeckittirat@gmail.com>
2026-04-05 16:46:55 -0400

    yes

--- python_visualizations/Total_Growth.py
+++ python_visualizations/Total_Growth.py
@@ -8,22 +8,32 @@ data = pd.read_csv('data/netflix_cleaned.csv')
 data = data.dropna(subset=['year_added'])
 data['year_added'] = data['year_added'].astype(int)
 
+# 2016 onward
+data = data[data['year_added'] >= 2015]
+
 growth = (
     data.groupby('year_added')
         .size()
         .reset_index(name='total_titles')
 )
-# line chart
+
 chart = alt.Chart(growth).mark_line(point=True).encode(
-    x=alt.X('year_added:O', title='Year Added'),
+    x=alt.X(
+        'year_added:Q',
+        title='Year Added',
+        axis=alt.Axis(format='d', tickMinStep=1, labelAngle=0)
+    ),
     y=alt.Y('total_titles:Q', title='Number of Titles'),
-    tooltip=['year_added', 'total_titles']
+    tooltip=[
+        alt.Tooltip('year_added:Q', title='Year', format='d'),
+        alt.Tooltip('total_titles:Q', title='Number of Titles')
+    ]
 ).properties(
     title='Total Netflix Content Growth Over Time',
     width=700,
     height=400
-)
+).interactive()
 
-# Save 
+# Save
 chart.save("python_visualizations/total_growth.html")
 print("Saved total_growth.html")
